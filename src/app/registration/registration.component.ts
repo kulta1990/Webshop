c3c15ce9e14eae51cd40735cb4f746059cfaa07d
Kulcsar Tamas <tamas.kulcsar30@gmail.com>
2023-02-26 19:43:10 +0100

    registration form fontstyle

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class RegistrationComponent {
 
+  Regexp():void {
+    const event = document.getElementById('btnReg');
+    event?.addEventListener('click', () => {
+      let a: any = document.getElementById('email') as HTMLElement | null;
+      let d: any = document.getElementById('password') as HTMLElement | null;
+      let e: any = document.getElementById('password_again') as HTMLElement | null;
+
+      let email: any = a.value;
+      let password: any = d.value;
+      let passwordAgain: any = e.value;
+console.log(email)
+      let regexEmail: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      let regexPasswort: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{5,}$/;
+
+
+      if (!(regexEmail.test(email))) {
+        alert('Not valid e-mail address!');
+      } else if (!(regexPasswort.test(password))) {
+       alert('The password must be min. 5 character and must contain min. 1 uppercase + min. 1 lowercase + sign!');
+      } else if (password !== passwordAgain) {
+        alert('The passwords do not match!');
+      }
+    })
+
+  }
+
 }
